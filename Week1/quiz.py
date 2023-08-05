fa48525d6ac1edc922f05f4363d09c87e78d5317
Michael Girma <michaelgirma@Michaels-MBP-2.fios-router.home>
2023-08-05 14:28:26 -0400

    week 1

---
+++ Week1/quiz.py
@@ -0,0 +1,39 @@
+print("My first Python program")
+
+
+print("Have a nice day")
+
+
+for i in range(5):
+  print("This is fun!")
+
+
+print("Programming in Python is fun!")
+
+
+ratio = ((1+(5**(1/2)))/2)
+print(ratio)
+
+
+# Enter code here:
+week = 7
+seconds = 86400
+
+print(seconds * week)
+
+# Should print 604800
+
+
+# Enter code here:
+possiblities = 10 ** 8
+print(possiblities) 
+
+# Should print 100000000
+
+
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size/sector_size
+
+
+print(sector_amount) # Should print 33554432.0
